badd1c78173659226d49ed03910fd4e1c4f4931f
Szymon Giermakowski <szymon.giermakowski@altium.com>
2024-06-03 08:22:17 +0200

    Added File logger

--- CustomPLMService/CustomPLMServiceExtensions.cs
+++ CustomPLMService/CustomPLMServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using CustomPLMService.Contract;
 using CustomPLMService.Contract.Models.Authentication;
 using CustomPLMService.Interceptors;
@@ -7,20 +8,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NReco.Logging.File;
+
 namespace CustomPLMService;
 
 [ExcludeFromCodeCoverage]
 public static class CustomPLMServiceExtensions
 {
-    public static IServiceCollection AddPLMServices<TMetadataService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(this IServiceCollection services) where TMetadataService : class, ICustomPlmMetadataService where TService : class, ICustomPlmService
+    public static IServiceCollection AddPLMServices<TMetadataService,
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TService>(
+        this IServiceCollection services) where TMetadataService : class, ICustomPlmMetadataService
+        where TService : class, ICustomPlmService
     {
-        services.AddLogging();
+        services.AddLogging(opt =>
+        {
+            opt.AddSimpleConsole(c => { c.TimestampFormat = "[HH:mm:ss] "; });
+            opt.AddFile("logs/GenericConnector_{0:yyyy}-{0:MM}-{0:dd}.log", fileLoggerOpts =>
+            {
+                fileLoggerOpts.UseUtcTimestamp = true;
+                fileLoggerOpts.FormatLogFileName = fName => string.Format(fName, DateTime.UtcNow);
+            });
+        });
         services.AddAutoMapper(typeof(PlmServiceMappingProfile));
 
-        services.AddGrpc(options =>
-        {
-            options.Interceptors.Add<UserContextInterceptor>();
-        });
+        services.AddGrpc(options => { options.Interceptors.Add<UserContextInterceptor>(); });
 
         services.AddScoped<IContext, Context>();
         services.AddScoped<ICustomPlmMetadataService, TMetadataService>();
@@ -41,10 +53,7 @@ public static class CustomPLMServiceExtensions
         app.UseRouting();
 
         app.UseMiddleware<GlobalExceptionHandler>();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapGrpcService<PlmServiceImpl>();
-        });
+        app.UseEndpoints(endpoints => { endpoints.MapGrpcService<PlmServiceImpl>(); });
         return app;
     }
 }
